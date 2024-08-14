9f8187a4b1d0992d49b44bca8b2753ca8fab1792
Ahmed-Yehia-Sokar <ahmedyehiasokar94@gmail.com>
2024-08-14 23:42:53 +0300

    Implementing the Bindable component.

---
+++ PickleRick/Common/Bindable/Bindable.swift
@@ -0,0 +1,23 @@
+//
+//  Bindable.swift
+//  PickleRick
+//
+//  Created by Admin on 14/08/2024.
+//
+
+import Foundation
+
+class Bindable<T> {
+    // MARK: - properties
+    var value: T? {
+        didSet {
+            observer?(value)
+        }
+    }
+    var observer: ((T?) -> ())?
+
+    // MARK: - public methods
+    func bind(observer: @escaping (T?) -> ()) {
+        self.observer = observer
+    }
+}
